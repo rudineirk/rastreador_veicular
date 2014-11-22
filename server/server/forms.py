b4e857eb9aa0841dba84d2a1213de51b304d0249
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2014-11-22 12:12:49 -0200

    Corrigido sintaxe

--- server/server/forms.py
+++ server/server/forms.py
@@ -3,17 +3,20 @@
 from __future__ import unicode_literals
 
 from django import forms
-from .models import Usuario, Rastreador, Localizacao,
+from .models import Usuario, Rastreador, Localizacao
+
 
 class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ['nome', 'senha', 'ativo']
 
+
 class RastreadorForm(forms.ModelForm):
     class Meta:
         model = Rastreador
         fields = ['nome', 'usuario']
+
 
 class LocalizacaoForm(forms.ModelForm):
     class Meta:
