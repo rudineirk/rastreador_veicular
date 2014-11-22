f2cdda91ccb39a503b1f0a7a5121d86fa01374ff
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2014-11-22 12:15:00 -0200

    Corrigido nome das tabelas e dos models

--- server/server/models.py
+++ server/server/models.py
@@ -3,48 +3,95 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import json
 
 
-class Usuario(models.Model):
+class User(models.Model):
     class Meta:
-        db_table = 'usuario'
+        db_table = 'user'
 
-    nome = models.CharField(max_length=50)
-    senha = models.CharField(max_length=30)
-    ativo = models.BooleanField()
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    active = models.BooleanField()
 
     def __str__(self):
-        return self.nome
+        return self.__unicode__()
 
     def __unicode__(self):
-        return unicode(self.nome)
+        return unicode(json.dumps({
+            'name': self.name,
+            'active': self.active,
+            'id': self.id
+        }, ensure_ascii=False))
+
+    def login(self, name, password):
+        try:
+            user = User.objects.filter(name=name).get()
+            if user.password == password:
+                return True
+            return False
+        except User.DoesNotExist:
+            return False
 
 
 class LogLogin(models.Model):
     class Meta:
         db_table = 'log_login'
         ordering = ['timestamp']
-    usuario = models.ForeignKey(Usuario)
+    user = models.ForeignKey(User)
     timestamp = models.DateTimeField(auto_now=True)
     status = models.IntegerField()
-    origem = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return unicode(json.dumps({
+            'user': self.user,
+            'timestamp': self.timestamp,
+            'status': self.status,
+            'origin': self.origin,
+        }, ensure_ascii=False))
 
 
-class Rastreador(models.Model):
+class Tracker(models.Model):
     class Meta:
-        db_table = 'rastreador'
+        db_table = 'tracker'
 
-    nome = models.CharField(max_length=50)
-    usuario = models.ForeignKey(Usuario)
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return unicode(json.dumps({
+            'name': self.name,
+            'user': self.user,
+        }, ensure_ascii=False))
 
 
-class Localizacao(models.Model):
+class Localization(models.Model):
     class Meta:
-        db_table = 'localizacao'
+        db_table = 'localization'
         ordering = ['timestamp']
 
-    posicao_lat = models.FloatField()
-    posicao_long = models.FloatField()
-    posicao_alt = models.FloatField()
+    pos_lat = models.FloatField()
+    pos_long = models.FloatField()
+    pos_alt = models.FloatField()
+    velocity = models.FloatField()
     timestamp = models.DateTimeField(auto_now=True)
-    rastreador = models.ForeignKey(Rastreador)
+    tracker = models.ForeignKey(Tracker)
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return unicode(json.dumps({
+            'pos_lat': self.pos_lat,
+            'pos_long': self.pos_long,
+            'pos_alt': self.pos_alt,
+            'timestamp': self.timestamp,
+            'tracker': self.tracker.id,
+        }, ensure_ascii=False))
