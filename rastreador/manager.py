40c8ded0edc60ea610c6d95487b61d68cb13ecc0
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2014-11-23 17:43:27 -0200

    Adicionado gerenciador de threads do rastreador

---
+++ rastreador/manager.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import Queue
+from threading import Thread
+import time
+from .communication import EnvioPosicao
+from .gpsmonitor import MonitoraPosicao
+
+
+TRACKER = 't1'
+SERVER_ADDR = 'http://192.168.1.50'
+
+class RastreadorVeicular(object):
+    def __init__(self):
+        self.queue = Queue.Queue()
+        self.monitora = MonitoraPosicao(self.queue)
+        self.envio_pos = EnvioPosicao(
+            self.queue,
+            tracker=TRACKER,
+            server_addr=SERVER_ADDR,
+        )
+
+    def run(self):
+        monitora_thread = Thread(target=self.monitora.verifica_gps)
+        envio_gps_thread = Thread(target=self.envio_pos.envia_pos)
+        monitora_thread.start()
+        envio_gps_thread.start()
+        while True:
+            if not monitora_thread.is_alive():
+                monitora_thread.run()
+            if not envio_gps_thread.is_alive():
+                envio_gps_thread.run()
+            time.sleep(0.5)
+
+
+def _main():
+    rastreador_veicular = RastreadorVeicular()
+    rastreador_veicular.run
+
+
+if __name__ == '__main__':
+    _main()
