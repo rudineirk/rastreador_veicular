ef2ae02d3d34d261526e2cd838b439dfe005a2be
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2014-11-23 16:55:29 -0200

    Adicionado envio da posição do rastreador via api

---
+++ rastreador/communication.py
@@ -0,0 +1,39 @@
+# -*- coding: utf8 -*-
+
+import json
+import requests
+import time
+
+
+class EnvioPosicao(object):
+    def __init__(self, queue, tracker='', server_addr=''):
+        self.queue = queue
+        self.server_addr = server_addr
+        self.tracker = tracker
+        self.url = self.get_url()
+
+    def envia_pos(self):
+        while True:
+            data = self.queue.get()
+            data = {
+                'pos_alt': data.altitude,
+                'pos_long': data.longitude,
+                'pos_lat': data.latitude,
+                'velocity': data.velocidade,
+                'timestamp': data.timestamp,
+            }
+            response = requests.post(self.url, data=json.dumps(data))
+            if not response.ok:
+                print('Failed ({0}): {1}\n'.format(
+                    response.status_code,
+                    response.text,
+                ))
+            time.sleep(0.01)
+
+    def get_url(self):
+        url = '/'.join([
+            self.server_addr,
+            'api/localization',
+            self.tracker,
+        ])
+        return url + '/'
