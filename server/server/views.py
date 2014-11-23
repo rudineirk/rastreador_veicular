f6437dffe309682282dac62dbb04713dd36134cc
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2014-11-22 22:48:37 -0200

    Corrigido views para usar os serializadores

--- server/server/views.py
+++ server/server/views.py
@@ -3,145 +3,158 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-def get_json_data(req_body):
+from rest_framework.decorators import api_view
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+from .models import User, Tracker, Localization
+from .serializers import UserSerializer, \
+    TrackerSerializer, \
+    LocalizationSerializer
+
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@api_view(['GET', 'POST'])
+def user_list(request):
+    if request.method == "GET":
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return JSONResponse(serializer.data)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user(request, pk=None):
     try:
-        req_json = json.loads(request.body)
-    except ValueError:
-        return None, RETORNOS['parametros_invalidos']
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return HttpResponse(status=404)
 
-    if len(req_json.keys()) <= 0:
-        return None, RETORNOS['parametros_invalidos']
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(user, data=data)
+        if not serializer.is_valid():
+            return JSONResponse(serializer.errors, status=400)
+        serializer.save()
+    elif request.method == 'DELETE':
+        user.delete()
+        return HttpResponse(status=204)
 
-    return req_json, None
+    return JSONResponse(serializer.data)
 
 
-def rastreador_get(request, pk=None):
-    if pk is None:
-        rastreadores = Rastreador.objects.all()
-        if not rastreadores:
-            return RETORNOS['nao_encontrado']
+@api_view(['GET', 'POST'])
+def tracker_list(request):
+    if request.method == "GET":
+        trackers = Tracker.objects.all()
+        serializer = TrackerSerializer(trackers, many=True)
+        return JSONResponse(serializer.data)
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = TrackerSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
 
-        rastreadores_data = []
-        for rastreador in rastreadores:
-            rastreadores_data.append({
-                'id': rastreador.id,
-                'nome': rastreador.nome,
-                'id_usuario': rastreador.usuario,
-            })
-        return [{'rastreadores': rastreadores_data}, 200]
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def tracker(request, pk=None):
     try:
-        rastreador = Rastreador.objects.get(id=pk)
-    except Rastreador.DoesNotExist:
-        return RETORNOS['nao_encontrado']
+        tracker = Tracker.objects.get(pk=pk)
+    except Tracker.DoesNotExist:
+        return HttpResponse(status=404)
 
-    return [{'id': rastreador.id,
-             'nome': rastreador.nome,
-             'id_usuario': rastreador.usuario}, 200]
+    if request.method == 'GET':
+        serializer = TrackerSerializer(tracker)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = TrackerSerializer(tracker, data=data)
+        if not serializer.is_valid():
+            return JSONResponse(serializer.errors, status=400)
+        serializer.save()
+    elif request.method == 'DELETE':
+        tracker.delete()
+        return HttpResponse(status=204)
+
+    return JSONResponse(serializer.data)
 
 
-def usuario_get(request, pk=None):
-    if pk is None:
-        usuarios = Usuario.objects.all()
-        if not usuarios:
-            return RETORNOS['nao_encontrado']
-        usuarios_data = []
-        for usuario in usuarios:
-            usuarios_data.append({
-                'id': usuario.id,
-                'nome': usuario.nome,
-                'ativo': usuario.ativo,
-            })
-        return [{'usuarios': usuarios_data}, 200]
+@api_view(['GET', 'POST'])
+def localization_list(request):
+    if request.method == "GET":
+        localizations = Localization.objects.all()
+        serializer = LocalizationSerializer(localization, many=True)
+        return JSONResponse(serializer.data)
+    if request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = LocalizationSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
 
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def localization(request, pk=None):
     try:
-        usuario = Usuario.objects.get(id=pk)
-    except Usuario.DoesNotExist:
-        return RETORNOS['nao_encontrado']
+        localization = Localization.objects.get(pk=pk)
+    except Localization.DoesNotExist:
+        return HttpResponse(status=404)
 
-    return [{'id': usuario.id,
-        'nome': usuario.nome,
-        'ativo': usuario.ativo}, 200]
+    if request.method == 'GET':
+        serializer = LocalizationSerializer(localization)
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = LocalizationSerializer(localization, data=data)
+        if not serializer.is_valid():
+            return JSONResponse(serializer.errors, status=400)
+        serializer.save()
+    elif request.method == 'DELETE':
+        localization.delete()
+        return HttpResponse(status=204)
+
+    return JSONResponse(serializer.data)
 
 
-def usuario_post(request, pk=None):
-    if pk is None:
-        return RETORNOS['parametros_invalidos']
-
-    req_json, status = get_json_data(request.body)
-    if status is not None:
-        return status
-
+@api_view(['GET'])
+def movement(request, user=None, tracker=None):
     try:
-        usuario = Usuario.objects.get(id=pk)
-    except Usuario.DoesNotExist:
-        return RETORNOS['nao_encontrado']
+        user = User.objects.get(name=user)
+        tracker = Tracker.objects.get(user=user, name=tracker)
+        localization = Localization.objects.all().order_by('-timestamp').get()
+    except (User.DoesNotExist,
+            Tracker.DoesNotExist,
+            Localization.DoesNotExist):
+        return HttpResponse(status=404)
 
+    resp = {
+        "movement": False,
+        "velocity": localization.velocity,
+        "pos_lat": localization.pos_lat,
+        "pos_long": localization.pos_long,
+        "pos_alt": localization.pos_alt,
+        "timestamp": localization.timestamp
+    }
 
+    if localization.velocity > 20:
+        resp['movement'] = True
 
-    if usuario.nome == request.get('nome'):
-
-
-    return [{'id': usuario.id,
-        'nome': usuario.nome,
-        'ativo': usuario.ativo}, 200]
-
-
-def usuario(request, pk=None):
-    if request.method == 'GET':
-        data = usuario_get(request, pk)
-    elif request.method == 'POST':
-        data = RETORNOS['nao_implementado']
-    elif request.method == 'DELETE':
-        data = RETORNOS['metodo_invalido']
-    elif request.method == 'PUT':
-        data = RETORNOS['metodo_invalido']
-    else:
-        data = RETORNOS['metodo_invalido']
-
-    return HttpResponse(json.dumps(data[0], ensure_ascii=False), status=data[1])
-
-
-def rastreador(request, pk=None):
-    if request.method == 'GET':
-        data = rastreador_get(request, pk)
-    elif request.method == 'POST':
-        data = RETORNOS['nao_implementado']
-    elif request.method == 'DELETE':
-        data = RETORNOS['metodo_invalido']
-    elif request.method == 'PUT':
-        data = RETORNOS['nao_implementado']
-    else:
-        data = RETORNOS['metodo_invalido']
-
-    return HttpResponse(json.dumps(data[0], ensure_ascii=False), status=data[1])
-
-
-def localizacao(request, pk=None):
-    if request.method == 'GET':
-        data = RETORNOS['nao_implementado']
-    elif request.method == 'POST':
-        data = RETORNOS['metodo_invalido']
-    elif request.method == 'DELETE':
-        data = RETORNOS['metodo_invalido']
-    elif request.method == 'PUT':
-        data = RETORNOS['nao_implementado']
-    else:
-        data = RETORNOS['metodo_invalido']
-
-    return HttpResponse(json.dumps(data[0], ensure_ascii=False), status=data[1])
-
-
-def movimentacao(request, pk=None):
-    if request.method == 'GET':
-        data = RETORNOS['nao_implementado']
-    elif request.method == 'POST':
-        data = RETORNOS['metodo_invalido']
-    elif request.method == 'DELETE':
-        data = RETORNOS['metodo_invalido']
-    elif request.method == 'PUT':
-        data = RETORNOS['metodo_invalido']
-    else:
-        data = RETORNOS['metodo_invalido']
-
-    return HttpResponse(json.dumps(data[0], ensure_ascii=False), status=data[1])
+    return JSONResponse(resp)
