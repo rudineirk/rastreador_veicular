d411ae8e700c2d020e76b69eca8a0cc9705cb40a
Rudinei Goi Roecker <rudinei.roecker@gmail.com>
2014-11-23 23:34:09 -0200

    Corrigido chamadas do gerenciador de threads

--- rastreador/manager.py
+++ rastreador/manager.py
@@ -4,8 +4,8 @@
 import Queue
 from threading import Thread
 import time
-from .communication import EnvioPosicao
-from .gpsmonitor import MonitoraPosicao
+from communication import EnvioPosicao
+from gpsmonitor import MonitoraPosicao
 
 
 SERIAL = 'A123456'
@@ -24,8 +24,11 @@ class RastreadorVeicular(object):
     def run(self):
         monitora_thread = Thread(target=self.monitora.verifica_gps)
         envio_gps_thread = Thread(target=self.envio_pos.envia_pos)
+        monitora_thread.daemon = True
+        envio_gps_thread.daemon = True
         monitora_thread.start()
         envio_gps_thread.start()
+
         while True:
             if not monitora_thread.is_alive():
                 monitora_thread.run()
@@ -36,7 +39,7 @@ class RastreadorVeicular(object):
 
 def _main():
     rastreador_veicular = RastreadorVeicular()
-    rastreador_veicular.run
+    rastreador_veicular.run()
 
 
 if __name__ == '__main__':
